69406b7a62b014b5488d777b56ee904c3e676c66
AndreaL <alafertteo@gmail.com>
2018-07-10 12:21:08 -0400

    Enviar e imprimir mensajes

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ window.onload = () =>{
       loggedIn.style.display = "none";
     }
   });
+
+  firebase.database().ref('messages')
+    .once('value')
+    .then((messages)=>{
+      console.log("Mensajes > "+JSON.stringify(messages));
+    })
+    .catch(()=>{
+
+    })
+
+  // Nuevos mensajes usando evento on child_added
+  firebase.database().ref('messages')
+    .on('child_added', (newMessage)=>{
+      messageContainer.innerHTML +=`
+        <p>Nombre : ${newMessage.val().creatorName}</p>
+        <p>${newMessage.val().text}</p>`;
+    })
 }
 
 function logInOrRegister() {
@@ -45,4 +62,44 @@ function logout() {
       console.log("Chao");
     })
     .catch();
+}
+
+function loginFacebook() {
+  const provider = new firebase.aut.FacebookAuthProvider();
+  // provider.addScope("user_birthday"); hay que pedirle permiso a facebook
+  provider.setCustomParameters({
+    'display':'popup'
+  });
+  firebase.auth().signInWithPopup(provider)
+  .then(()=>{
+    console.log("Login con Facebook")
+  })
+  .catch((error)=>{
+    console.log("Error de firebase > "+error.code);
+    console.log("Error de firebase, mensaje > "+error.message);
+  })
+}
+
+// Firebase database
+// Guardar lo que se vende en laboratoria
+//Producto, quien lo vende, el valor, una descripción, foto, tock disponible
+// usuario
+// número
+
+// Cantidad que se compra, precio total, comprdor, vendedor, cuando se compró
+// Transacción
+
+// Usaremos una colección para guardar los mensajes, llamada messages
+function sendMessage(){
+  const currentUser = firebase.auth().currentUser;
+  const  messageAreaText = messageArea.value;
+
+  // Para tener una nueva llave en la colección messages
+  const newMessageKey = firebase.database().ref().child('messages').push().key;
+
+  firebase.database().ref(`messages/${newMessageKey}`).set({
+    creator : currentUser.uid,
+    creatorName : currentUser.displayName,
+    text : messageAreaText,
+  });
 }
